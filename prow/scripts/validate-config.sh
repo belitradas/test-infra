7492fa403a90fa196e1ab40924144ab0029beb09
belitre <miguelterron@gmail.com>
2019-05-22 00:50:56 +0100

    added script to validate

--- prow/scripts/validate-config.sh
+++ prow/scripts/validate-config.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-cd $HOME/go/src/k8s.io/test-infra/hack/
+K8S_TEST_INFRA_PATH="${K8S_TEST_INFRA_PATH:-${HOME}/go/src/k8s.io/test-infra/hack/}"
+
+cd $K8S_TEST_INFRA_PATH
 
 bazel run //prow/cmd/checkconfig -- --plugin-config=$HOME/go/src/github.com/belitradas/test-infra/prow/plugins.yaml --config-path=$HOME/go/src/github.com/belitradas/test-infra/prow/config.yaml
