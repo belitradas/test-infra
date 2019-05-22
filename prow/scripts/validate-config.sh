38b2487065b5e09b9e30834596106e65b5aa3770
belitre <miguelterron@gmail.com>
2019-05-22 01:12:27 +0100

    fix script and remove arguments

--- prow/scripts/validate-config.sh
+++ prow/scripts/validate-config.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-K8S_TEST_INFRA_PATH="${K8S_TEST_INFRA_PATH:-${HOME}/go/src/k8s.io/test-infra/hack/}"
+K8S_TEST_INFRA_PATH="${K8S_TEST_INFRA_PATH:-${HOME}/go/src/k8s.io/test-infra/hack}"
+MY_PATH="${MY_PATH:-$HOME/go/src/github.com/belitradas/test-infra}"
 
 cd $K8S_TEST_INFRA_PATH
 
-bazel run //prow/cmd/checkconfig -- --plugin-config=$HOME/go/src/github.com/belitradas/test-infra/prow/plugins.yaml --config-path=$HOME/go/src/github.com/belitradas/test-infra/prow/config.yaml
+bazel run //prow/cmd/checkconfig -- --plugin-config=$MY_PATH/prow/plugins.yaml --config-path=$MY_PATH/prow/config.yaml --job-config-path=$MY_PATH/config/jobs
